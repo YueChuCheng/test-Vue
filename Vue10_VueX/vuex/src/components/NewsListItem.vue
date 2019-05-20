7b7f6e506d2a6677384985913a3ccf175680e62f
Yue Chu Cheng <maggie9907@gmail.com>
2019-05-20 16:17:50 +0800

    add VueX

---
+++ Vue10_VueX/vuex/src/components/NewsListItem.vue
@@ -0,0 +1,27 @@
+<template>
+    <li class="list-group-item news__item">
+       
+            <!-- @click設定發⽣生點擊 事件時就呼叫onNewsSelect函數  -->
+        <p @click="onNewsSelect" class="news__title">{{news.title}}</p>
+        
+    </li>
+</template>
+
+<script>
+export default {
+    name: 'NewsListItem',
+    props: ['news'],
+    methods:{
+        //利利⽤用this.$emit函數發出⼀一個叫做 newsSelect的⾃自訂事件，並且附帶 送出本則新聞的內容this.news 
+        onNewsSelect(){
+            this.$emit('newsSelect',this.news);
+        }
+    }
+}
+</script>
+
+<style lang="postcss" scoped>
+@import "../styles/base/_variables.css";
+@import "../styles/base/_global.css";
+@import "../styles/modules/_section.css";
+</style>

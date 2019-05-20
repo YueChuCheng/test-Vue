0aa366ce44c0c1192fcafb80fed39d689948f048
Yue Chu Cheng <maggie9907@gmail.com>
2019-05-20 16:37:06 +0800

    set vuex_formate

--- Vue10_VueX/vuex/src/main.js
+++ Vue10_VueX/vuex/src/main.js
@@ -1,7 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+//只要指引到store目錄，vue就會自 動尋找index.js並且將index.js的內容 設定為store 
+import store from './store';
+
 
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
 
---
+++ Vue10_VueX/vuex/src/store/modules/googleNews.js
@@ -0,0 +1,25 @@
+//state: 儲存元件的狀態變數 
+const state={
+
+}
+
+//getters: 計算元件的輸出變數 
+const getters={
+
+}
+
+// actions: 變更更狀態變數的函數(可能 數個actions對應到相同的mutations)
+const actions={
+
+}
+
+//mutations(突變): 計算狀態變數的函數 
+const mutations={
+
+}
+export default{
+    state,
+    getters,
+    actions,
+    mutations
+}
